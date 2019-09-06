73baf0e1a6e2c3faa5c952deec66ce5241f05cfb
Jana <sjana@internal.tavisca.com>
2019-09-06 16:58:24 +0530

    Added response object for all REST transactions

--- LibraryApi/Service/Response.cs
+++ LibraryApi/Service/Response.cs
@@ -1,43 +1,21 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 
 namespace LibraryApi.Service
 {
-    public static class Response
+    public class Response
     {
-        public const string BaseUri = "C:/Users/sjana/source/repos/LibraryApi/LibraryApi/response.json";
+        public Object Model { get; set; }
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
 
-        public static dynamic GetObject(string baseUri = BaseUri)
+        public List<string> ErrorList;
+
+        public Response()
         {
-            WebClient client = new WebClient();
-            string jsonType = File.ReadAllText(baseUri);
-            var resultString = client.DownloadString(baseUri);
-            dynamic resultObject = JsonConvert.DeserializeObject(jsonType);
-            return resultObject;
-        }
-
-        public static dynamic GetErrorObject(int code,string message)
-        {
-            dynamic responseObject = GetObject();
-            responseObject.Response.Error.code = code;
-            responseObject.Response.Error.message = message;
-            responseObject.Response.Model = null;
-            return responseObject;
-
-        }
-
-        public static dynamic GetSuccessObject(string model)
-        {
-            dynamic responseObject = GetObject();
-            responseObject.Response.Error = null;
-            responseObject.Response.Model = model;
-            return responseObject;
-
+            ErrorList = new List<string>();
         }
     }
 }
